98b06b9d89064f3b6da0a7c12a9913b375f09c2c
dipace <codeict.dipesh@gmail.com>
2025-11-30 14:53:39 +0545

    only zod schema for more flexibility

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Form from "@rjsf/shadcn";
 import { RJSFSchema } from "@rjsf/utils";
-import validator from "@rjsf/validator-ajv8";
+import ReactJson from "react-json-view";
 import * as z from "zod";
 
 const todoSchema = z.object({
@@ -11,39 +10,14 @@ const todoSchema = z.object({
 
 const jsonSchema = z.toJSONSchema(todoSchema) as RJSFSchema;
 
-const log = (type: any) => console.log.bind(console, type);
-
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
-const TITLE_TEXT = `
- ██████╗ ███████╗████████╗████████╗███████╗██████╗
- ██╔══██╗██╔════╝╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗
- ██████╔╝█████╗     ██║      ██║   █████╗  ██████╔╝
- ██╔══██╗██╔══╝     ██║      ██║   ██╔══╝  ██╔══██╗
- ██████╔╝███████╗   ██║      ██║   ███████╗██║  ██║
- ╚═════╝ ╚══════╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝
-
- ████████╗    ███████╗████████╗ █████╗  ██████╗██╗  ██╗
- ╚══██╔══╝    ██╔════╝╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝
-    ██║       ███████╗   ██║   ███████║██║     █████╔╝
-    ██║       ╚════██║   ██║   ██╔══██║██║     ██╔═██╗
-    ██║       ███████║   ██║   ██║  ██║╚██████╗██║  ██╗
-    ╚═╝       ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
- `;
-
 function HomeComponent() {
   return (
-    <div className="container mx-auto max-w-3xl px-4 py-2">
-      <pre className="overflow-x-auto font-mono text-sm">{TITLE_TEXT}</pre>
-      <Form
-        schema={jsonSchema}
-        validator={validator}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")}
-      />
+    <div className="container mx-auto max-w-3xl px-4 py-2 my-20">
+      <ReactJson src={jsonSchema} displayDataTypes={false} />
     </div>
   );
 }
