646a6cb2d662d5a60ca1c741462a237577ab74d8
dipace <codeict.dipesh@gmail.com>
2025-12-01 14:07:45 +0545

    just tunelling server

--- apps/server/src/grpc.ts
+++ apps/server/src/grpc.ts
@@ -3,26 +3,13 @@ import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 import { TunnelHandlers } from "./grpc-tunnel-server";
 
-const PROTO_PATH_MAIN = path.resolve(process.cwd(), "proto/service.proto");
 const PROTO_PATH_TUNNEL = path.resolve(process.cwd(), "proto/tunnel.proto");
-
-const defMain = protoLoader.loadSync(PROTO_PATH_MAIN);
 const defTunnel = protoLoader.loadSync(PROTO_PATH_TUNNEL);
 
-const mainPkg = grpc.loadPackageDefinition(defMain) as any;
 const tunnelPkg = grpc.loadPackageDefinition(defTunnel) as any;
 
 const server = new grpc.Server();
 
-// Existing service stays!
-server.addService(mainPkg.example.ExampleService.service, {
-  SayHello: (call: any, callback: any) => {
-    const name = call.request.name || "Guest";
-    callback(null, { message: `Hello, ${name}! from grpc server` });
-  },
-});
-
-// Add tunnel here!
 server.addService(tunnelPkg.tunnel.TunnelService.service, TunnelHandlers);
 
 export const startGRPCServer = () => {
